e0458d790bf93bbd4340450fa58e9eabb9454320
trabnett <timrabnett@gmail.com>
2019-07-19 12:27:17 -0400

    fetching words

--- src/App.js
+++ src/App.js
@@ -10,25 +10,36 @@ class App extends React.Component{
     this.state = {
       score: 0,
       timeup: false,
-      countdown: 12
+      countdown: 12,
+      word: "Hey"
     }
   }
   handleScore = () =>{
     this.setState({score: this.state.score + 1}, () => console.log(this.state.score))
   }
+  restart = () => {
+    console.log("restart")
+    this.setState({countdown: 30, score: 0, timeup: false})
+  }
   tick = () => {
     this.state.countdown > 0 ? this.setState({countdown: this.state.countdown - 1}, () => console.log(this.state.countdown)) : this.setState({countdown: 0, timeup: true})
   }
-
+  componentDidMount() {
+    fetch('https://random-word-api.herokuapp.com/word?key=A03EJMGY&number=20', {mode: 'cors'}, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    })
+    .then(res => res.text()) // OR res.json()
+    .then(res => console.log("here it is", res))
+  }
   render(){
     return (
       <div className="container">
-        {this.state.timeup ? <p>time up</p> : null}
-        <Timer key={this.state.countdown} tick={this.tick} time={this.state.countdown}/>
+        {this.state.timeup ? <p className="text-centered">time up</p> : <Timer key={this.state.countdown} tick={this.tick} time={this.state.countdown}/>}
         <div>
           {this.state.score}
         </div>
-        <TypeArea handleScore={this.handleScore}/>
+        <TypeArea handleScore={this.handleScore} restart={this.restart} word={this.state.word} timeup={this.state.timeup}/>
       </div>
     );
   }
--- src/TypeArea.js
+++ src/TypeArea.js
@@ -6,22 +6,23 @@ class TypeArea extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-
+            textArea: ""
         }
     }
-
+    handleChange = (e) => {
+        e.target.value === this.props.word && !this.props.timeup ? this.props.handleScore() : console.log("hell0")
+        this.setState({textArea: e.target.value})
+        console.log(e.target.value)
+    }
     render(){
         return(
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Enter your </Form.Label>
-                    <Form.Control type="Text" placeholder="Type in the word!" />
-                    <Form.Text className="text-muted">
-                    Enter your word here.
-                    </Form.Text>
+                    <Form.Control value={this.state.textArea} onChange={this.handleChange} type="Text" placeholder="Type in the word!" />
                 </Form.Group>
-                <Button variant="primary" type="button" onClick={this.props.handleScore}>
-                    Submit
+                <Button variant="primary" type="button" onClick={this.props.timeup ? this.props.restart : this.props.handleScore}>
+                    {this.props.timeup ? "Restart" : "Submit"}
                 </Button>
             </Form>
         )
